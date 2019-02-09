e19b9116b292d0941ca5f29abb6798fd519faaf5
coltonmack <31513534+coltonmack@users.noreply.github.com>
2019-02-09 15:10:00 -0500

    Initial Proj
    Here we go

---
+++ FacePay/FacePay/FacePay/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  FacePay
+//
+//  Created by Colton Mack on 2/9/19.
+//  Copyright © 2019 FacePayers. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
